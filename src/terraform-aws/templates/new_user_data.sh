30aa33f946bd43c62a66f86680b06bb41a714ab4
dan <dan@danmaxic.com>
2019-02-20 15:06:03 +0200

    זסההס

--- src/terraform-aws/templates/new_user_data.sh
+++ src/terraform-aws/templates/new_user_data.sh
@@ -154,80 +154,13 @@ function installPythonEsRally(){
   pip3 install esrally
 }
 
-function configAwsLogs(){
-    echo "±±±±±±±±±±±±±>configAwsLogs"
-    cat > /etc/awslogs/awslogs.conf <<- EOF
-[general]
-state_file = /var/lib/awslogs/agent-state
-use_gzip_http_content_encoding = true
-
-[/var/log/dmesg]
-file = /var/log/dmesg
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-
-[/var/log/messages]
-file = /var/log/messages
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-datetime_format = %b %d %H:%M:%S
-
-[/var/log/docker]
-file = /var/log/docker
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-datetime_format = %Y-%m-%dT%H:%M:%S.%f
-
-[/var/log/ecs/ecs-init.log]
-file = /var/log/ecs/ecs-init.log.*
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-
-[/var/log/ecs/ecs-agent.log]
-file = /var/log/ecs/ecs-agent.log.*
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-
-[/var/log/ecs/audit.log]
-file = /var/log/ecs/audit.log.*
-log_group_name = $${ECS_CLOUDWATCH_LOG_GROUP}
-log_stream_name = $${ECS_CLUSTER_NAME}/{container_instance_id}
-datetime_format = %Y-%m-%dT%H:%M:%SZ
-EOF
-    sed -i -e "s/region = us-east-1/region = $current_aws_region/g" /etc/awslogs/awscli.conf
-
-    # Set the ip address of the node
-    container_instance_id=$(curl 169.254.169.254/latest/meta-data/local-ipv4)
-    sed -i -e "s/$${container_instance_id}/$${container_instance_id}/g" /etc/awslogs/awslogs.conf
-
-    cat > /etc/init/awslogjob.conf <<- EOF
-    #upstart-job
-    description "Configure and start CloudWatch Logs agent on Amazon ECS container instance"
-    author "Amazon Web Services"
-    start on started ecs
-    script
-        exec 2>>/var/log/ecs/cloudwatch-logs-start.log
-        set -x
-
-        until curl -s http://localhost:51678/v1/metadata
-        do
-            sleep 1
-        done
-
-        service awslogs start
-        chkconfig awslogs on
-    end script
-EOF
-}
 #=============== BASIC ROUTINES
 
 function InstallBasics(){
   installPreReq
   installJava
   installKibana
-  installElastic
+#  installElastic
 }
 
 
